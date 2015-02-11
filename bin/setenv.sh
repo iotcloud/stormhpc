811aedc53b3be6c9f965f55419455b4f8c8ec136
supunkamburugamuva <supun06@gmail.com>
2015-02-11 14:28:38 -0500

    fixing the sed command

--- bin/setenv.sh
+++ bin/setenv.sh
@@ -4,7 +4,7 @@
 export STORMHPC_HOME="/N/u/skamburu/software/stormhpc"
 
 # Set this to the location of the Storm installation
-export STORM_HOME="/N/u/skamburu/software/apache-storm-0.9.3"
+export STORM_HOME="/N/u/skamburu/software/apache-storm-0.9.2"
 
 # Set this to the location you want to use for storm local dir
 export STORM_LOCAL_DIR="/tmp/$HOSTNAME/local-dir"
